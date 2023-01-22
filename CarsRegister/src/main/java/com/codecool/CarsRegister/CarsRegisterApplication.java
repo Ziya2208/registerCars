56ab6c95e0ae5c64eb62de2926520d41b0c92df6
Ziya2208x <ziyoss68@gmail.com>
2023-01-22 14:21:42 +0100

    Added Backend by Spring Initializr

---
+++ CarsRegister/src/main/java/com/codecool/CarsRegister/CarsRegisterApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.CarsRegister;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarsRegisterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarsRegisterApplication.class, args);
+	}
+
+}
---
+++ CarsRegister/src/test/java/com/codecool/CarsRegister/CarsRegisterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecool.CarsRegister;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarsRegisterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
