70fecaa32b48f4fddc45e65cc2ef063d337e2e4c
Ziya2208 <ziyoss68@gmail.com>
2025-04-13 21:23:22 +0200

    updated, removed tailwind test

--- CarsRegister/carsregsiter_frontend/src/App.jsx
+++ CarsRegister/carsregsiter_frontend/src/App.jsx
@@ -1,13 +1,11 @@
 
 import './App.css';
 import  Car  from './components/Car'
-import TailwindTest from './components/Tailwindtest';
 
 function App() {
   return (
     <div className="App">
     <Car/>
-    <TailwindTest/>
     </div>
   );
 }
